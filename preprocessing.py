d001b188679ce49f8c8ca539a07f7501246d0b67
Kshitij Shresth <kshitijshresth@icloud.com>
2024-08-15 14:31:26 +0300

    Update preprocessing.py
    originally genres was in a dictionary format, we changed it to list

--- preprocessing.py
+++ preprocessing.py
@@ -1,2 +1,12 @@
 movies = movies.merge(credits, on = 'title')
 #merging the two datasets on the basis of title
+movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+#dropping unnecessary features that are of no use or skew the prediction 
+movies.dropna(inplace = True)
+def dlist(x):
+  list = []
+  for i in ast.literal_eval(x):
+    list.append(i['name'])
+    return list
+movies['genres']=movies['genres'].apply(dlist)
+#originally genres was in a dictionary format, we changed it to list     
