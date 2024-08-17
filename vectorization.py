ac89e671472b6a3403d95c0f8bad274bb9f19d87
Kshitij Shresth <kshitijshresth@icloud.com>
2024-08-17 17:30:17 +0300

    Update vectorization.py

--- vectorization.py
+++ vectorization.py
@@ -1,14 +1,14 @@
-from nltk.stem.porter import PorterStemmer
-cv = CountVectorizer(max_features = 5000, stop_words = 'english')
-df['tags_str'] = df['tags'].apply(lambda x: ' '.join(x))
-vectors = cv.fit_transform(df['tags_str']).toarray()
-from nltk.stem.porter import PorterStemmer
 ps = PorterStemmer()
-
 def stem(text):
-  y = []
-  for i in text.split():
-    y.append(ps.stem(i))
+    y = []
+    for i in text.split():
+        y.append(ps.stem(i))
+    return " ".join(y)
+movies['tags'] = movies['tags'].apply(stem)
+#reducing words to their root form 
 
-    a = "".join(y)
-    return a
+tfidf = TfidfVectorizer(max_features=5000, stop_words='english')
+vectors = tfidf.fit_transform(movies['tags']).toarray()
+
+similarity = cosine_similarity(vectors)
+#Performing vectorization using TF-IDF
