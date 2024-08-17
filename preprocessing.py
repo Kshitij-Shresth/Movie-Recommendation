c7bb5a0d6e450fe40d487dd1d5297c3bd99fee03
Kshitij Shresth <kshitijshresth@icloud.com>
2024-08-17 17:29:13 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -3,44 +3,40 @@ credits = pd.read_csv('tmdb_5000_credits.csv')
 movies = movies.merge(credits, on = 'title')
 #merging the two datasets on the basis of title
 movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+movies.dropna(inplace=True)
 #dropping unnecessary features that are of no use or skew the prediction 
-movies.dropna(inplace = True)
+
 def dlist(x):
-  list = []
-  for i in ast.literal_eval(x):
-    list.append(i['name'])
-    return list
-movies['genres']=movies['genres'].apply(dlist)
-#originally genres was in a dictionary format, we changed it to list     
+    list_ = []
+    try:
+        for i in ast.literal_eval(x):
+            list_.append(i['name'])
+    except (ValueError, SyntaxError):
+        return []
+    return list_
+
+movies['genres'] = movies['genres'].apply(dlist)
 movies['keywords'] = movies['keywords'].apply(dlist)
 movies['cast'] = movies['cast'].apply(dlist)
 movies['crew'] = movies['crew'].apply(dlist)
-#converting movies overview to list to make concantenation easier (we will be making a tags column)
-movies['overview'] = movies['overview'].apply(lambda x:x.split())
 
-#removing spaces is good for reccomender system, people with same first name might confuse the system
+#originally genres was in a dictionary format, we changed it to list     
+def get_director(x):
+    try:
+        for i in ast.literal_eval(x):
+            if i['job'] == 'Director':
+                return i['name']
+    except (ValueError, SyntaxError):
+        return ''
+    return ''
+movies['director'] = movies['crew'].apply(get_director)
+#extracting the directors name from crew
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+movies['genres'] = movies['genres'].apply(lambda x: [i.replace(" ", "") for i in x])
+movies['keywords'] = movies['keywords'].apply(lambda x: [i.replace(" ", "") for i in x])
+movies['cast'] = movies['cast'].apply(lambda x: [i.replace(" ", "") for i in x])
+movies['director'] = movies['director'].apply(lambda x: x.replace(" ", "") if isinstance(x, str) else '')
 
-movies['genres'] = movies['genres'].apply(
-    lambda x: [i.replace(" ", "") for i in x] if x else []
-)
-
-movies['keywords'] = movies['keywords'].apply(
-    lambda x: [i.replace(" ", "") for i in x] if x else []
-)
-
-movies['cast'] = movies['cast'].apply(
-    lambda x: [i.replace(" ", "") for i in x] if x else []
-)
-
-movies['crew'] = movies['crew'].apply(
-    lambda x: [i.replace(" ", "") for i in x] if x else []
-)
-
-#creating tags column in a new dataframe
-movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
-
-df = movies[['movie_id', 'title', 'tags']]
-#vectorization (converting tag text to vector) and reccommending the closest vectors
-cv = CountVectorizer(max_features = 5000, stop_words = 'english')
-df['tags_str'] = df['tags'].apply(lambda x: ' '.join(x))
-vectors = cv.fit_transform(df['tags_str']).toarray()
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['director'].apply(lambda x: [x])
+movies['tags'] = movies['tags'].apply(lambda x: " ".join(x))
+#Concantenating features after stemming and removing spaces
