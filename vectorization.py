8e3f4cd907f83f8b3829771a881fe8d3ecf350ad
Kshitij Shresth <kshitijshresth@icloud.com>
2024-08-17 10:50:31 +0300

    Update vectorization.py

--- vectorization.py
+++ vectorization.py
@@ -1,3 +1,14 @@
+from nltk.stem.porter import PorterStemmer
 cv = CountVectorizer(max_features = 5000, stop_words = 'english')
 df['tags_str'] = df['tags'].apply(lambda x: ' '.join(x))
 vectors = cv.fit_transform(df['tags_str']).toarray()
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+
+def stem(text):
+  y = []
+  for i in text.split():
+    y.append(ps.stem(i))
+
+    a = "".join(y)
+    return a
