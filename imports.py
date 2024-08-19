375e28723cf6abab6b54c555fe3d6a409be4a09b
Kshitij Shresth <kshitijshresth@icloud.com>
2024-08-19 10:02:52 +0300

    Update imports.py

--- imports.py
+++ imports.py
@@ -3,3 +3,7 @@ import numpy as np
 import ast
 from sklearn.feature_extraction.text import CountVectorizer
 import nltk 
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.stem.porter import PorterStemmer
